37af24b1274211c9af0ae2f5d1ba6f7b9d2c4a5d
Amritanshu Joshi <amritanshujoshi@gmail.com>
2015-12-06 01:32:54 -0500

    Classifier and File Creator

--- src/yelp/ArffFileCreator.java
+++ src/yelp/ArffFileCreator.java
@@ -10,22 +10,35 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
+
+import weka.core.Attribute;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
 
 public class ArffFileCreator {
 
-	public void fileCreator(File f, BufferedWriter arffWriter, BufferedWriter csvWriter)
-			throws IOException {
+	public void fileCreator(File f, File arff, BufferedWriter arffWriter,
+			BufferedWriter csvWriter) throws Exception {
+
+		FastVector attributes, attributeType, categoryT;
+		Instances data;
 
 		BufferedReader br1 = new BufferedReader(new FileReader(f));
 
 		String s = "";
-		HashMap<String, Integer> wordStatus = new HashMap<String, Integer>();
+		ArrayList<String> wordStatus = new ArrayList<String>();
 		HashMap<String, Integer> categoryMap = new HashMap<String, Integer>();
 		String[] words = new String[3];
 		String word, sub, category;
-		int counter = 0, categoryCount = 0, cat;
+		int counter = 0, categoryCount = 0;
+
+		attributes = new FastVector();
+		attributeType = new FastVector();
+		attributeType.addElement("0");
+		attributeType.addElement("1");
 
 		while ((s = br1.readLine()) != null) {
 			sub = s.substring(s.indexOf("[") + 1, s.indexOf("]"));
@@ -34,40 +47,26 @@ public class ArffFileCreator {
 				for (int i = 0; i < words.length; i++) {
 					word = words[i].substring(words[i].indexOf("\"") + 1,
 							words[i].lastIndexOf("\""));
-					if (word.length() < 3)
-						continue;
-					if (word.contains("'")) {
-						word = word.replace("'", "\\'");
-						word = "\'" + word + "\'";
-					}
-					else
-						word = "\'" + word + "\'";
-					if (!wordStatus.containsKey(word) && word.length() > 1) {
-						wordStatus.put(word, 0);
+					if (!wordStatus.contains(word) && word.length() > 1) {
+						wordStatus.add(word);
+						attributes
+								.addElement(new Attribute(word, attributeType));
 					}
 				}
 			}
 		}
 
 		System.out.println(wordStatus.size());
-
-		// Writing relation name in both arff files
-		arffWriter.write("@relation reviewtips\n\n");
-
-		// Writing attributes in both arff files
-		for (String att : wordStatus.keySet()) {
-			arffWriter.write("@attribute " + att + " {0,1}\n");
-		}
-		
 		BufferedReader br2 = new BufferedReader(new FileReader(f));
-		
+		categoryT = new FastVector();
+
 		while ((s = br2.readLine()) != null) {
-			
+
 			sub = s.substring(s.indexOf(":\":") + 4);
 
 			// Takes out category from json file
 			category = sub.substring(0, sub.indexOf("\""));
-			
+
 			/*
 			 * Checks if a category is present in category map If yes, then it
 			 * doesn't put it again otherwise it does.
@@ -75,90 +74,87 @@ public class ArffFileCreator {
 			if (!categoryMap.containsKey(category)) {
 				categoryCount++;
 				categoryMap.put(category, categoryCount);
-				csvWriter.write(category+","+categoryCount+"\n");
+				csvWriter.write(category + "," + categoryCount + "\n");
+				categoryT.addElement(Integer.toString(categoryCount));				
 			}
 		}
-		
-		String categoryType = "{";
-		
-		for (Map.Entry<String, Integer> entry : categoryMap.entrySet())
-			categoryType += entry.getValue()+",";
 
-		
-		categoryType += categoryType.substring(0, categoryType.lastIndexOf(","))+"}";
-		
-		arffWriter.write("@attribute 'category' "+categoryType+"\n");
-		arffWriter.write("\n@data\n");	
+		attributes.addElement(new Attribute("category", categoryT));
+
+		data = new Instances("TipsReviewsCategories", attributes, 0);
 
 		System.out.println("Populating hashmap and creating file...");
-		
+
 		BufferedReader br3 = new BufferedReader(new FileReader(f));
 
 		while ((s = br3.readLine()) != null) {
 
+			double[] vals = new double[data.numAttributes()];
+
+			for (int i = 0; i < vals.length; i++)
+				vals[i] = attributeType.indexOf("0");
+
 			// Just to check how many rows have been added to file
-			if (counter % 10000 == 0)
+			if (counter % 500 == 0)
 				System.out.println(counter + " data rows written");
-			counter++;
+
+			if (counter == 20000) {
+				arffWriter.write(data.toString());
+				arffWriter.close();
+//				IncrementalClassifier.classify(data);
+				System.exit(0);
+			}
 
 			sub = s.substring(s.indexOf(":\":") + 4);
-
+			
 			// Takes out category from json file
 			category = sub.substring(0, sub.indexOf("\""));
-
-			// Temporary map: To store map of words for each category
-			HashMap<String, Integer> tempMap = new HashMap<String, Integer>();
-			tempMap.putAll(wordStatus);
 
 			/*
 			 * Extracts each noun word from the line and updates the word map
 			 * accordingly. So, for each category there will be only 3 words
 			 * which will be 1 others will be 0
 			 */
-			sub = s.substring(s.indexOf("[") + 1, s.indexOf("]"));
-			if (sub.length() > 0) {
-				words = sub.split(",");
-				for (int i = 0; i < words.length; i++) {
-					word = words[i].substring(words[i].indexOf("\"") + 1,
-							words[i].lastIndexOf("\""));
-					if (word.length() < 3)
-						continue;
-					if (word.contains("'")) {
-						word = word.replace("'", "\\'");
-						word = "\'" + word + "\'";
+			try {
+				sub = s.substring(s.indexOf("[") + 1, s.indexOf("]"));
+				if (sub.length() > 0) {
+					words = sub.split(",");
+					for (int i = 0; i < words.length; i++) {
+						word = words[i].substring(words[i].indexOf("\"") + 1,
+								words[i].lastIndexOf("\""));
+						if(wordStatus.indexOf(word) < vals.length && word.length() > 1)
+							vals[wordStatus.indexOf(word)] = attributeType
+								.indexOf("1");
 					}
-					tempMap.put(word, 1);
+					vals[vals.length-1] = categoryMap.get(category)-1;					
+					data.add(new Instance(1.0, vals));
+					counter++;
 				}
+			} catch (NullPointerException e) {
+				e.printStackTrace();
 			}
-
-			/*
-			 * Puts each category and associated values into the file
-			 */
-			for (Map.Entry<String, Integer> entry : tempMap.entrySet()) {
-				arffWriter.write(entry.getValue() + ",");
-			}
-			arffWriter.write(categoryMap.get(category) + "\n");
 		}
 
 		br1.close();
 		br2.close();
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 
 		File f = new File(
 				"D:\\Lectures\\Fall 15\\Search\\ProjectFiles\\pos.json");
-		BufferedWriter arffWriter = new BufferedWriter(
-				new FileWriter(
-						new File(
-								"D:\\Lectures\\Fall 15\\Search\\ProjectFiles\\reviewtipscategory.arff")));
+
+		File arff = new File(
+				"D:\\Lectures\\Fall 15\\Search\\ProjectFiles\\reviewtipscategory.arff");
+
+		BufferedWriter arffWriter = new BufferedWriter(new FileWriter(arff));
 		BufferedWriter csvWriter = new BufferedWriter(
 				new FileWriter(
 						new File(
 								"D:\\Lectures\\Fall 15\\Search\\ProjectFiles\\categoryMap.csv")));
 
 		ArffFileCreator fc = new ArffFileCreator();
-		fc.fileCreator(f, arffWriter, csvWriter);
+		fc.fileCreator(f, arff, arffWriter, csvWriter);
 		System.out.println("Done!!");
 		arffWriter.close();
 		csvWriter.close();
---
+++ src/yelp/IncrementalClassifier.java
@@ -0,0 +1,78 @@
+package yelp;
+
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayesUpdateable;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+/**
+ * This example trains NaiveBayes incrementally on data obtained from the
+ * ArffLoader.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class IncrementalClassifier {
+
+	/**
+	 * Expects an ARFF file as first argument (class attribute is assumed to be
+	 * the last attribute).
+	 *
+	 * @param args
+	 *            the commandline arguments
+	 * @throws Exception
+	 *             if something goes wrong
+	 */
+	public static void classify(Instances data) throws Exception {
+		
+		BufferedWriter bw = new BufferedWriter(new FileWriter(new File(
+				"D:\\Lectures\\Fall 15\\Search\\ProjectFiles\\Classification.txt")));
+		// load data
+		ArffLoader loader = new ArffLoader();
+		loader.setFile(new File(
+				"D:\\Lectures\\Fall 15\\Search\\ProjectFiles\\reviewtipscategory.arff"));
+		Instances structure = loader.getStructure();
+		structure.setClassIndex(structure.numAttributes() - 1);
+
+		System.out.println("Structure Loaded!! Started Training....");
+
+		// train NaiveBayes
+		NaiveBayesUpdateable nb = new NaiveBayesUpdateable();
+		nb.buildClassifier(structure);
+		Instance current;
+		while ((current = loader.getNextInstance(structure)) != null)
+			nb.updateClassifier(current);
+		
+		evaluate(nb, data);
+
+		 System.out.println("Trained!! " + "Started Writing....");
+		// output generated model
+		// System.out.println(nb);
+		 bw.write(nb.toString());
+		 bw.close();
+
+	}
+
+	public static void evaluate(NaiveBayesUpdateable nb, Instances data)
+			throws Exception {
+
+		System.out.println("Evaluating...");
+		BufferedWriter bw = new BufferedWriter(new FileWriter(new File(
+				"D:\\Lectures\\Fall 15\\Search\\ProjectFiles\\Evaluation.txt")));
+		data.setClassIndex(data.numAttributes()-1);
+		Evaluation eval = new Evaluation(data);
+		eval.crossValidateModel(nb, data, 10, new Random(1));
+		System.out.println("Writing Evaluation...");
+		System.out.println(eval.toSummaryString("\nResults\n=====\n", true));
+		bw.write(eval.fMeasure(1) + " " + eval.precision(1) + " "
+				+ eval.recall(1));
+		bw.close();
+
+	}
+}
